7cd894018596c7763b5d66554fbf2d4e0a8198b0
regisskate <60084851+regisskate@users.noreply.github.com>
2022-05-25 10:29:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Recicle</title>
+      </Head>
+      <h1>
+        Hello World!
+      </h1>
+    </>
+  )
+}
