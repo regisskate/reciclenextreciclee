abb6605781815522eda443100239a47b2ff80c84
Reginaldo <freestreetskate@gmail.com>
2022-06-14 11:06:13 -0300

    refatorando

---
+++ src/components/ButtonAglutinador/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from './styles.module.scss';
+import Modal from "react-modal"
+
+export function ButtonAglutinador() {
+
+  const [newProductionRecordOpenModal, setNewProductionRecordOpenModal] = useState(false);
+  function handleOpenNewProductionRecordModal() {
+    setNewProductionRecordOpenModal(true);
+  }
+
+  function handleCloseNewProductionRecordModal() {
+    setNewProductionRecordOpenModal(false)
+  }
+  return (
+    <main className={styles.aglutinadorContainer}>
+      <section className={styles.aglutinadorContent}>
+        <button type="button" onClick={handleOpenNewProductionRecordModal}>
+          <img src="/images/aglutinador.svg" alt="aglutinador" />
+          <span>Aglutinador</span>
+        </button>
+
+        <Modal
+          isOpen={newProductionRecordOpenModal}
+          onRequestClose={handleCloseNewProductionRecordModal}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
+        >
+          <button type="button" onClick={handleCloseNewProductionRecordModal} className="react-modal-close">
+            <img src="/images/close.svg" alt="fechar modal" />
+          </button>
+          <h1>Aqui vai ser o formulário</h1>
+          <p>
+            Data <br></br>
+            <input type="date" name="Data" className="form-control" /><br></br>
+            tipo de material<br></br>
+            <button type="button" className="cores">Cores</button>
+            <button type="button" className="strech">Strech</button>
+            <button type="button" className="azul">Azul</button>
+            <button type="button" className="pvdc">Pvdc</button> <br></br>
+
+            Inicio
+            <input
+              type="time"
+              name="Inicio"
+            /><br></br>
+            fim
+            <input type="time" name="Fim" />
+          </p>
+          <button type="submit">Enviar</button>
+        </Modal>
+      </section>
+    </main>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
 import styles from './home.module.scss';
 import Head from 'next/head';
+import { ButtonAglutinador } from '../components/ButtonAglutinador';
+import { ButtonPrensa } from '../components/ButtonPrensa';
 
 
 
 
 export default function Home() {
-  const [newProductionRecordOpenModal, setNewProductionRecordOpenModal] = useState(false);
-  const [sendProduction, setSendProduction] = useState('send');
-  function handleOpenNewProductionRecordModal() {
-    setNewProductionRecordOpenModal(true);
-  }
 
-  function handleCloseNewProductionRecordModal() {
-    setNewProductionRecordOpenModal(false)
-  }
   return (
     <>
       <Head>
@@ -23,44 +15,9 @@ export default function Home() {
       </Head>
       <main className={styles.homeContainer}>
         <section className={styles.homeContent}>
-          <button type="button" onClick={handleOpenNewProductionRecordModal}>
-            <img src="/images/aglutinador.svg" alt="aglutinador" />
-            <span>Aglutinador</span>
-          </button>
-          <button type="button" onClick={handleOpenNewProductionRecordModal}>
-            <img src="/images/prensa.svg" alt="prensa" />
-            <span>Prensa</span>
-          </button>
+          <ButtonAglutinador />
+          <ButtonPrensa />
 
-          <Modal
-            isOpen={newProductionRecordOpenModal}
-            onRequestClose={handleCloseNewProductionRecordModal}
-            overlayClassName="react-modal-overlay"
-            className="react-modal-content"
-          >
-            <button type="button" onClick={handleCloseNewProductionRecordModal} className="react-modal-close">
-              <img src="/images/close.svg" alt="fechar modal" />
-            </button>
-            <h1>Aqui vai ser o formulário</h1>
-            <p>
-              Data <br></br>
-              <input type="date" name="Data" className="form-control" /><br></br>
-              tipo de material<br></br>
-              <button type="button" className="cores">Cores</button>
-              <button type="button" className="strech">Strech</button>
-              <button type="button" className="azul">Azul</button>
-              <button type="button" className="pvdc">Pvdc</button> <br></br>
-
-              Inicio
-              <input
-                type="time"
-                name="Inicio"
-              /><br></br>
-              fim
-              <input type="time" name="Fim" />
-            </p>
-            <button type="submit">Enviar</button>
-          </Modal>
         </section>
       </main>
     </>
